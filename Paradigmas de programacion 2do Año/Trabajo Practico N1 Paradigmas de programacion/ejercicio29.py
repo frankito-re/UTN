d816749c9e44b054e41d2ff0a49f0f03c29153f3
Franco Reyes <d3ksur@Francos-MacBook-Pro.local>
2025-03-18 11:37:17 -0300

    master auto-commit

---
+++ Paradigmas de programacion 2do Año/Trabajo Practico N1 Paradigmas de programacion/ejercicio29.py
@@ -0,0 +1,10 @@
+# Obtener un cuento corto, desde un archivo de texto llamado cuento.txt (Busque algun
+# parrafo o cuento corto clasico y guardelo en un archivo de texto con dicho nombre).
+# Realizar el siguiente analisis y mostrar en pantalla un informe de:
+# a) Cuantas palabras contiene en total.
+# b) Cuantas frases contiene en total. (¿Como debera distinguir una frase de otra?)
+# c) Cuantos nombres propios o nombres de lugares hay en total.
+# d) El total de vocales que contiene ese cuento.
+# e) Cuantas palabras de longitud par existen.
+# f ) Cuantas palabras de longitud impar contiene.
+
