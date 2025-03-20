ffa294c8727857f36f6541b03007fa17099550fc
Franco Reyes <d3ksur@Francos-MacBook-Pro.local>
2025-03-20 17:27:56 -0300

    master auto-commit

--- Paradigmas de programacion 2do Año/Trabajo Practico N1 Paradigmas de programacion/ejercicio29.py
+++ Paradigmas de programacion 2do Año/Trabajo Practico N1 Paradigmas de programacion/ejercicio29.py
@@ -7,4 +7,52 @@
 # d) El total de vocales que contiene ese cuento.
 # e) Cuantas palabras de longitud par existen.
 # f ) Cuantas palabras de longitud impar contiene.
+import string
 
+file = open("/Users/d3ksur/Proyectos Mios/UTN/Paradigmas de programacion 2do Año/Trabajo Practico N1 Paradigmas de programacion/cuento.txt", "r")
+cuento = file.read()
+
+splited_words = cuento.split()
+
+# Cuantas palabras contiene en total
+print(f'La cantidad de palabras que hay son: {len(splited_words)}')
+
+# Cuantas frases contiene en total. (¿Como debera distinguir una frase de otra?)
+phrases_counter = 0
+for l in cuento:
+    if l == '.':
+        phrases_counter += 1
+print(f'La cantidad de frases que hay es: {phrases_counter}')
+
+# Cuantos nombres propios o nombres de lugares hay en total.
+name_city_counter = 0
+for l in cuento:
+    if l.isupper():
+        name_city_counter+=1
+    elif l == '.' and name_city_counter > 0:
+        name_city_counter-=1
+print(f'La cantidad de nombres propios o nombres de lugares que hay es: {name_city_counter}')
+
+# El total de vocales que contiene ese cuento.
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+vowels_counter = 0
+for l in cuento:
+    if l in vowels:
+        vowels_counter += 1
+print(f'La cantidad de vocales que hay es: {vowels_counter}')
+
+# Cuantas palabras de longitud par existen.
+pair_lenth_words_counter = 0
+odd_lenth_words_counter = 0
+
+for w in splited_words:
+    clean_word = w.strip(string.punctuation)
+    if len(clean_word) % 2 == 0:
+        pair_lenth_words_counter+=1
+    else:
+        odd_lenth_words_counter+=1
+print(f'La cantidad de palabras de longitud par que hay son: {pair_lenth_words_counter}')
+
+#Cuantas palabras de longitud impar contiene.
+print(f'La cantidad de palabras de longitud impar que hay son: {odd_lenth_words_counter}')          
