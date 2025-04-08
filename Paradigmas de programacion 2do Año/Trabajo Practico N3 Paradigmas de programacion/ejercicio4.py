e6a7fed87885981e1a132018354db5d5423cd236
Franco Reyes <d3ksur@Francos-MacBook-Pro.local>
2025-04-08 20:53:12 -0300

    master auto-commit

--- Paradigmas de programacion 2do Año/Trabajo Practico N3 Paradigmas de programacion/ejercicio4.py
+++ Paradigmas de programacion 2do Año/Trabajo Practico N3 Paradigmas de programacion/ejercicio4.py
@@ -1,8 +1,35 @@
 # Nombre alumno: Franco Genaro Reyes
-
-# Crear un Objeto Temperatura. Debera tener como metodos, la posibilidad de conversion
-# a diferentes temperaturas, ademas debera permitir definir cual sera la temperatura por
-# defecto que utilizara. Grados Celcius, Farenheit, Kelvins, etc.
 class Temperatura:
-    def __init__(self):
-        pass
+    def __init__(self, valor, unidad='C'):
+        self.valor = valor
+        self.unidad = unidad.upper()
+    
+    def conversion_unidad(self, destino):
+        destino = destino.upper()
+        if self.unidad == 'C':
+            if destino == 'F':
+                return (self.valor * 9/5) + 32
+            elif destino == 'K':
+                return self.valor + 273.15
+            elif destino == 'C':
+                return self.valor
+        elif self.unidad == 'F':
+            if destino == 'C':
+                return (self.valor - 32) * 5/9
+            elif destino == 'K':
+                return ((self.valor - 32) * 5/9) + 273.15
+            elif destino == 'F':
+                return self.valor
+        elif self.unidad == 'K':
+            if destino == 'C':
+                return self.valor - 273.15
+            elif destino == 'F':
+                return ((self.valor - 273.15) * 9/5) + 32
+            elif destino == 'K':
+                return self.valor
+        else:
+            return ValueError("Unidad no reconocida.")
+
+conversion_C_K = Temperatura(30, 'C')
+print(conversion_C_K.conversion_unidad('F'))
+print(conversion_C_K.conversion_unidad('K'))
