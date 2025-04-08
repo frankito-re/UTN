b27043b8ab53cbee6169d0dff86a55682c93ec3f
Franco Reyes <d3ksur@Francos-MacBook-Pro.local>
2025-04-08 20:56:08 -0300

    master auto-commit

---
+++ Paradigmas de programacion 2do Año/Trabajo Practico N3 Paradigmas de programacion/ejercicio5.py
@@ -0,0 +1,5 @@
+# Nombre alumno: Franco Genaro Reyes
+
+# Crear el objeto Empleado, debera determinar cuales serian sus atributos y metodos, considerando que se necesitara calcular su salario, y por lo tanto, informacion como horas
+# trabajadas y tarifa por hora, seran importantes.
+
