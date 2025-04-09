7312833df2ac6257de7b525a3ed3f359cc7f75df
Franco Reyes <d3ksur@Francos-MacBook-Pro.local>
2025-04-08 21:04:55 -0300

    master auto-commit

--- Paradigmas de programacion 2do Año/Trabajo Practico N3 Paradigmas de programacion/ejercicio5.py
+++ Paradigmas de programacion 2do Año/Trabajo Practico N3 Paradigmas de programacion/ejercicio5.py
@@ -1,5 +1,9 @@
 # Nombre alumno: Franco Genaro Reyes
 
-# Crear el objeto Empleado, debera determinar cuales serian sus atributos y metodos, considerando que se necesitara calcular su salario, y por lo tanto, informacion como horas
+# Crear el objeto Empleado, debera determinar cuales serian sus atributos y metodos,
+# considerando que se necesitara calcular su salario, y por lo tanto, informacion como horas
 # trabajadas y tarifa por hora, seran importantes.
 
+class Empleado:
+    def __init__(self):
+        pass
---
+++ README.md
@@ -0,0 +1,4 @@
+Este material está destinado para facilitar el estudio y servir como referencia para futuros estudiantes.
+Puedes clonar o descargar el repositorio de la siguiente manera:
+```bash
+git clone https://github.com/frankito-re/UTN.git
