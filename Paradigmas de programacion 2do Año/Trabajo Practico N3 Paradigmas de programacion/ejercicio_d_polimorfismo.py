cdc364b77f0fbbab0b5ecae8097ab5df7491b354
Franco Reyes <d3ksur@Francos-MacBook-Pro.local>
2025-04-29 09:14:31 -0300

    master auto-commit

---
+++ Paradigmas de programacion 2do Año/Trabajo Practico N3 Paradigmas de programacion/ejercicio_d_polimorfismo.py
@@ -0,0 +1,3 @@
+# Utilizando las clases implementadas en el practico anterior para vectores de 2 y 3
+# componentes, extiendalas ahora, para que los operadores aritmeticos convenientes,
+# sirvan con objetos del tipo vector
