bb5b7dfac7320c98096e177140becdd8c41cf334
Franco Reyes <d3ksur@Francos-MacBook-Pro.local>
2025-06-25 10:03:01 -0300

    master auto-commit

--- Paradigmas de programacion 2do Año/Trabajo Practico N7 Paradigmas de programacion/ejercicio6.java
+++ Paradigmas de programacion 2do Año/Trabajo Practico N7 Paradigmas de programacion/ejercicio6.java
@@ -1,12 +1,26 @@
-// Dibujar en pantalla una piramide de caracteres X, dada la longitud de la base ingresada
-// por el usuario. Ejemplo: el usuario ingresa 5, entonces debera verse
-// X
-// XXX
-// XXXXX
 
 // Nombre alumno: Franco Genaro Reyes
+import java.util.Scanner;
+
 public class ejercicio6 {
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
-                
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ingrese la longitud de la base: ");
+        int longitud = input.nextInt();
+
+        for (int i = 0; i < longitud; i++) {
+            int numX = 2 * i + 1;
+            int numEspacios = longitud - i - 1;
+
+            for (int j = 0; j < numEspacios; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < numX; j++) {
+                System.out.print("X");
+            }
+            System.out.println();
+        }
+        input.close();
     }
 }
