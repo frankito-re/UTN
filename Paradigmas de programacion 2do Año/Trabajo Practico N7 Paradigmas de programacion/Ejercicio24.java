0cd13f5dc6dd623ad6197f5b143de93ca4e2a1bf
Franco Reyes <d3ksur@Francos-MacBook-Pro.local>
2025-06-29 12:10:15 -0300

    master auto-commit

--- Paradigmas de programacion 2do Año/Trabajo Practico N7 Paradigmas de programacion/Ejercicio24.java
+++ Paradigmas de programacion 2do Año/Trabajo Practico N7 Paradigmas de programacion/Ejercicio24.java
@@ -1,5 +1,23 @@
-public class Ejercicio24 {
-    public static void main(String[] args) {
 
+// Nombre alumno: Franco Genaro Reyes
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ejercicio24 {
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        ArrayList<String> palabras = new ArrayList<>(List.of("hola", "chau"));
+        ArrayList<String> resultado = repetir_elemento(palabras, 2);
+        System.out.println(resultado);
+    }
+
+    static ArrayList<String> repetir_elemento(ArrayList<String> array, int cantidad_veces) {
+        ArrayList<String> resultado = new ArrayList<>();
+        for (String elemento : array) {
+            for (int i = 0; i < cantidad_veces; i++) {
+                resultado.add(elemento);
+            }
+        }
+        return resultado;
     }
 }
