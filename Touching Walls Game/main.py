6e19c66b6ba518aa6c12c64a0a05083611998f9f
Franco Reyes <d3ksur@Francos-MacBook-Pro.local>
2025-09-13 12:29:43 -0300

    master auto-commit

--- Touching Walls Game/main.py
+++ Touching Walls Game/main.py
@@ -2,21 +2,46 @@ import pygame
 import random
 import math
 import os
+import settings
 
-from os import listdir
-from os.path import isfile, join
-
-from player import Player
+from os.path import join
 
 pygame.init()
 
 pygame.display.set_caption("Touching Walls Game")
 
-WIDTH, HEIGHT = 1000, 800
-FPS = 60
-PLAYER_VEL = 5
+window = pygame.display.set_mode((settings.WINDOW_WIDTH, settings.WINDOW_HEIGHT))
 
-window = pygame.display.set_mode((WIDTH, HEIGHT))
+from os import listdir
+from os.path import isfile, join
+
+import pygame
+
+def flip(sprites):
+    return [pygame.transform.flip(sprite,True, False) for sprite in sprites]
+
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+    
+    all_sprites= {}
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+        
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i*width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+
+            if direction:
+                all_sprites[image.replace(".png", "") + "_right"] = sprites
+                all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+            else:
+                all_sprites[image.replace(".png", "")] = sprites
+            
+    return all_sprites
 
 def get_background(name):
     image = pygame.image.load(join("assets", "Background", name))
@@ -24,8 +49,8 @@ def get_background(name):
     print(image.get_rect())
     tiles = []
 
-    for i in range(WIDTH // width + 1):
-        for j in range(HEIGHT // height + 1):
+    for i in range(settings.WINDOW_WIDTH // width + 1):
+        for j in range(settings.WINDOW_HEIGHT // height + 1):
             pos = [i * width, j * height]
             tiles.append(pos)
 
@@ -39,19 +64,68 @@ def draw(window, background, bg_image, player):
 
     pygame.display.update()
 
+def handle_move(player):
+    keys = pygame.key.get_pressed()
+    
+    player.x_vel = 0
+    if keys[pygame.K_LEFT]:
+        player.move_left(settings.PLAYER_VEL)
+    if keys[pygame.K_RIGHT]:
+        player.move_right(settings.PLAYER_VEL)
+
+class Player(pygame.sprite.Sprite):
+    COLOR = (255, 0, 0)
+    SPRITES = load_sprite_sheets("character", "Character-No-Weapon", 100, 100, True)
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+        self.fall_count = 0
+        
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+    
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+        
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
+
+    def loop(self, fps):
+        # self.y_vel += min(1, (self.fall_count/fps) * self.GRAVITY)
+        self.move(self.x_vel, self.y_vel)
+        
+        self.fall_count += 1
+    
+    def draw(self, win):
+        self.sprite = self.SPRITES['Character-NoWeapon-Idle_' + self.direction][0]
+        win.blit(self.sprite, (self.rect.x, self.rect.y))
+
 def main(window):
     clock = pygame.time.Clock()
     background, bg_image = get_background("Blue.png")
     player = Player(100, 100, 50, 50)
     run = True
     while run:
-        clock.tick(FPS)
+        clock.tick(settings.FPS)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
-
+        
+        player.loop(settings.FPS)
+        handle_move(player)
         draw(window, background, bg_image, player)
 
 if __name__ == "__main__":
